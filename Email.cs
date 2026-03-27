e769962c8808e18fe5460015173aa09d8a2c2c75
ccords-cmd <ccords@anamosa.k12.ia.us>
2026-03-27 11:31:59 -0500

    Fix formatting issues in Email class

--- Email.cs
+++ Email.cs
@@ -2,6 +2,5 @@
 public class Email
 {
     public string introduction = "oibgoidfbhgoiufhbg fougbs\'rougb'\, \"sdoigub\", \n" + 
-        "\tousdgbsfuigb \\" +
-        "\\"
+        "\tousdgbsfuigb \\\"
 }
